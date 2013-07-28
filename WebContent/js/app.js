2ff2c680bfa3d7a97c23055a4c14a07e7f9aa6ac
Jon Clark <jon@fake.server.com>
2013-07-28 16:37:44 -0400

    angular partials working

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,14 +1,10 @@
 'use strict';
 
-//var IndividualList = $resource('https://api.cigna.com/services/v1/individuals/me?access_token=19265589-52e5-4a62-9e20-26533913609e');
-//var list;
-
-angular.module('myApp', ['ngResource', 'individualListServices']);
-angular.module('myApp2', ['ngResource', 'biometricServices']);
-
-/*config(['$routeProvider', function($routeProvider) {
-$routeProvider.
-    when('/phones', {templateUrl: 'partials/phone-list.html', controller: IndividualListCtrl}).
-    otherwise({redirectTo: '/phones'});
-}]);
-*/
+angular.module('myApp', ['ngResource', 'individualListServices', 'biometricServices'])
+	.config(function($routeProvider) {
+	    $routeProvider.
+	      when('/', {controller:biometricListCtrl, templateUrl:'individual.html'}).
+	      when('/person', {controller:biometricListCtrl, templateUrl:'individual.html'}).
+	      when('/list', {controller:individualListCtrl, templateUrl:'individuals.html'}).
+	      otherwise({redirectTo:'/'});
+	  });
